eae7efc79619e43a8a7e71d95f99ffa0026e4126
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-06 12:53:44 -0300

    resolvendo os exercicios de rexExp

--- expressao-regular/index6.js
+++ expressao-regular/index6.js
@@ -10,16 +10,14 @@
 // + um ou mais {1,}  ele vai começa pegar um Quantificador de 1 a n
 // modificadores g-global: i: case-incentive:  m-multline: checar tudo
 
-//let regExp = /^\(\d{2}\)\s\d{4}-\d{4}$/;
-//let telefone1 = "(48) 9408-5402";
+regExp = /^(\d{2}\/){2}\d{4}$/;
+let data = "20/03/2020";
+console.log(regExp.test(data));
 
-// let regExp = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/;
-// let regExp = /\d{3}\.\d{3}\.\d{3}\-\d{2}/;
-let regExp = /^(\d{3}\.){2}\d{3}\-\d{2}$/g;
-
+regExp = /^(\d{3}\.){2}\d{3}\-\d{2}$/;
 let cpf = "051.503.831-80";
 console.log(regExp.test(cpf));
 
-const re = /\d+?/;
-let nome = "1abc";
-console.log(re.exec(nome));
+regExp = /^\d{1}(.\d{3}){2}$/;
+let rg = "3.171.373";
+console.log(regExp.test(rg));
