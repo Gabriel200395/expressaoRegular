dd78ad0679aed9b78e4b7b60f976ce0c79a1db81
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-06 11:07:45 -0300

    validando cpf

--- expressao-regular/index5.js
+++ expressao-regular/index5.js
@@ -10,6 +10,12 @@
 // + um ou mais
 // modificadores g-global: i: case-incentive:  m-multline
 
-let regExp = /^\(\d{2}\)\s\d{4}-\d{4}$/;
-let telefone1 = "(48) 9408-5402";
-console.log(regExp.exec(telefone1));
+//let regExp = /^\(\d{2}\)\s\d{4}-\d{4}$/;
+//let telefone1 = "(48) 9408-5402";
+
+// let regExp = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/;
+// let regExp = /\d{3}\.\d{3}\.\d{3}\-\d{2}/;
+let regExp = /(\d{3}\.){2}\d{3}\-\d{2}/;
+
+let cpf = "051.503.831-80";
+console.log(regExp.exec(cpf));
